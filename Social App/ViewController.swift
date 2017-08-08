861c03da20230707a0b867b6ca99a9814e5229a8
Punpun <anki@protonmail.com>
2017-08-08 10:53:49 +0200

    Initial Commit

---
+++ Social App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social App
+//
+//  Created by Stefan Markovic on 8/8/17.
+//  Copyright © 2017 Stefan Markovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
